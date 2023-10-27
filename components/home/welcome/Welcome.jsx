2423372e65f5743f3619eac70ef7a1bcd8bbc11c
DmitriyAngve <kirirch129@gmail.com>
2023-10-27 15:24:17 +0700

    #

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+// View - это просто <div>. Text - это просто <p>
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
   return (
     <View>
       <Text>Welcome</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
