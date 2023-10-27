59a6cc9c11d0c2d10d8bcc27f5ee36f685dc87ca
DmitriyAngve <kirirch129@gmail.com>
2023-10-27 16:03:59 +0700

    #

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+// "expo-splash-screen" - предоставляет ф-ии для контроля отображения экрана-заставки. "preventAutoHideAsync()" - предотвращает автоматическое скрытие экранаа заставки.
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
-};
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
 export default Layout;
